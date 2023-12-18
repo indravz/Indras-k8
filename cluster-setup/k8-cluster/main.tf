9b954e6e068a7b65c024b04c5e208ee5f4f09ec0
reddyin <indrareddy.eee@gmail.com>
2023-12-18 17:30:57 -0600

    Add day 1 commands

--- cluster-setup/k8-cluster/main.tf
+++ cluster-setup/k8-cluster/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "k8_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = [var.security_group_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
